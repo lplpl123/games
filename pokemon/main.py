8052827d5faa7b0d787d010db10b55b3e55095a3
lplpl123 <758826560@qq.com>
2023-03-23 17:56:09 +0800

    202303231756

--- pokemon/main.py
+++ pokemon/main.py
@@ -1,20 +1,38 @@
 import pygame
+import sys
 
 
 # 初始化
 pygame.init()
 # 设置主屏窗口
 screen = pygame.display.set_mode((240, 160))
-# 设置窗口标题
 pygame.display.set_caption('pokemon')
-# 设置背景
-# bg = pygame.image.load("./maps/map01.png") # todo
-
+# 加载地图
+map = pygame.image.load('./data/graphic/maps/map01.png')
+# 加载人物
+character_image = pygame.image.load('./data/graphic/characters/character01.png')
+# todo 要把哪些是需要加载的，统一先加载在一个list数据库里面
+# 控制fps
 clock = pygame.time.Clock()
 # run
 while True:
+    site = [0, 0]
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYUP:
+            site[1] -= 1
+        if event.type == pygame.K_DOWN:
+            site[1] += 1
+        if event.type == pygame.K_LEFT:
+            site[0] -= 1
+        if event.type == pygame.K_RIGHT:
+            site[0] += 1
     # 控制为30fps
     clock.tick(30)
     # 绘制背景图片
-    # screen.blit(bg, (0, 0)) # todo
+    screen.blit(map, (0, 0)) # todo
+    # 绘制人物
+    screen.blit(character_image, (50, 50), (5, 5, 20, 25))
     pygame.display.flip()
