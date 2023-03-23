618803b0dd7aa704cd717d90e060d0a9bb37fc12
lplpl123 <758826560@qq.com>
2023-03-23 12:01:18 +0800

    202303231201

--- pokemon/main.py
+++ pokemon/main.py
@@ -4,11 +4,11 @@ import pygame
 # 初始化
 pygame.init()
 # 设置主屏窗口
-screen = pygame.display.set_mode((1920, 1080))
+screen = pygame.display.set_mode((240, 160))
 # 设置窗口标题
 pygame.display.set_caption('pokemon')
 # 设置背景
-bg = pygame.image.load("./maps/map01.png")
+# bg = pygame.image.load("./maps/map01.png") # todo
 
 clock = pygame.time.Clock()
 # run
@@ -16,5 +16,5 @@ while True:
     # 控制为30fps
     clock.tick(30)
     # 绘制背景图片
-    screen.blit(bg, (0, 0))
+    # screen.blit(bg, (0, 0)) # todo
     pygame.display.flip()
